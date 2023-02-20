ba41f783a8359d4529220cb37e0022f32ea1dea1
almaz <almaz8811@gmail.com>
2023-02-20 23:43:19 +0300

    Работа с шаблонизатором Jinja2

--- template/jinja_2.py
+++ template/jinja_2.py
@@ -10,5 +10,6 @@ rendered_page = template.render(cap1_title='Красная кепка',
                                 cap1_text='$ 100.00',
                                 cap2_title='Черная кепка',
                                 cap2_text=f'$ {price}.00')
+print(rendered_page)
 with open('index.html', 'w', encoding="utf8") as file:
     file.write(rendered_page)
