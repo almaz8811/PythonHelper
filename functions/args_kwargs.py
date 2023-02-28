a765e9790f5c5c85ff46f4f5a47fbeb81491b590
almaz <almaz8811@gmail.com>
2023-02-28 15:18:12 +0300

    Работа с *args и **kwargs

--- functions/args_kwargs.py
+++ functions/args_kwargs.py
@@ -4,6 +4,27 @@ d, *e = 1, 2, 3
 i, *j, k = 'abcde'
 l, *_, m = 'abcde'  # не хранит переменную _ (удобно использовать вместо срезов)
 
+
+# / - все, что слева - это позиционные аргументы
+# *, - все, что слева - это keywords (ключевые) аргументы
+def example(a, b, /, c, *, d):
+    print(a)
+    print(b)
+    print(c)
+    print(d)
+
+
+# args собирает все позиционные аргументы в кортеж
+def my_print(*args, number=1):
+    for arg in args:
+        print(str(arg) * number)
+
+# kwargs собирает все keyword аргументы в словарь
+def my_print_2(*args, **kwargs):
+    print(f'Аргументы kwargs: {kwargs}')
+    for arg in args:
+        print(str(arg), **kwargs)   # можно передать распакованные аргументы в другую функцию
+
 if __name__ == '__main__':
     print(f'{a=}')  # a=1
     print(f'{b=}')  # b=2
@@ -24,3 +45,8 @@ if __name__ == '__main__':
     print(f'{m=}')  # m='e'
 
     print(*[1, 2, 3])  # 1 2 3 - распаковка значений
+
+    example(1, 2, c=3, d=4)
+    print('================================================================')
+    my_print(1, 2, 3, 4, number=2)
+    my_print_2(1, 2, 3, 4, sep=':', end='-')
