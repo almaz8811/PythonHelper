89c85a3416135eadbab158567094b8ba25e86b70
almaz <almaz8811@gmail.com>
2023-03-15 00:34:51 +0300

    Тесты на исключения

--- tests/test/calculator_test.py
+++ tests/test/calculator_test.py
@@ -15,6 +15,31 @@ class CalculatorTests(TestCase):
     def test_divide(self):
         self.assertEqual(calculator('74/2'), 37.0)
 
+    def test_no_signs(self):
+        with self.assertRaises(ValueError) as e:
+            calculator('abracadabra')
+        self.assertEqual('Выражение должно содержать хотя бы один знак (+-*/)', e.exception.args[0])
+
+    def test_two_signs(self):
+        with self.assertRaises(ValueError) as e:
+            calculator('2+2+2')
+        self.assertEqual('Выражение должно содержать 2 целых числа и один знак', e.exception.args[0])
+
+    def test_many_signs(self):
+        with self.assertRaises(ValueError) as e:
+            calculator('2+3+20')
+        self.assertEqual('Выражение должно содержать 2 целых числа и один знак', e.exception.args[0])
+
+    def test_no_ints(self):
+        with self.assertRaises(ValueError) as e:
+            calculator('2.3+3.0')
+        self.assertEqual('Выражение должно содержать 2 целых числа и один знак', e.exception.args[0])
+
+    def test_strings(self):
+        with self.assertRaises(ValueError) as e:
+            calculator('a+b')
+        self.assertEqual('Выражение должно содержать 2 целых числа и один знак', e.exception.args[0])
+
 
 if __name__ == '__main__':
     main()
