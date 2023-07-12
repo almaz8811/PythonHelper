7d213b26dffcd95efbb17f81f4fd776c972ca346
almaz <almaz8811@gmail.com>
2023-07-12 23:50:52 +0300

    aiogram расположение и генерация кнопок

--- stepik/aiogram/keyboard/less_9.1.3_position_button.py
+++ stepik/aiogram/keyboard/less_9.1.3_position_button.py
@@ -10,20 +10,21 @@ dp: Dispatcher = Dispatcher()
 
 # Пример 1 - Создаем клавиатуру из 3-х рядов кнопок по 3 кнопки в каждом ряду
 # Создаем объекты кнопок
-# button_1: KeyboardButton = KeyboardButton(text='Кнопка 1')
-# button_2: KeyboardButton = KeyboardButton(text='Кнопка 2')
-# button_3: KeyboardButton = KeyboardButton(text='Кнопка 3')
-# button_4: KeyboardButton = KeyboardButton(text='Кнопка 4')
-# button_5: KeyboardButton = KeyboardButton(text='Кнопка 5')
-# button_6: KeyboardButton = KeyboardButton(text='Кнопка 6')
-# button_7: KeyboardButton = KeyboardButton(text='Кнопка 7')
-# button_8: KeyboardButton = KeyboardButton(text='Кнопка 8')
-# button_9: KeyboardButton = KeyboardButton(text='Кнопка 9')
+button_1: KeyboardButton = KeyboardButton(text='Кнопка 1')
+button_2: KeyboardButton = KeyboardButton(text='Кнопка 2')
+button_3: KeyboardButton = KeyboardButton(text='Кнопка 3')
+button_4: KeyboardButton = KeyboardButton(text='Кнопка 4')
+button_5: KeyboardButton = KeyboardButton(text='Кнопка 5')
+button_6: KeyboardButton = KeyboardButton(text='Кнопка 6')
+button_7: KeyboardButton = KeyboardButton(text='Кнопка 7')
+button_8: KeyboardButton = KeyboardButton(text='Кнопка 8')
+button_9: KeyboardButton = KeyboardButton(text='Кнопка 9')
 # Создаем объект клавиатуры, добавляя в него кнопки
-# my_keyboard: ReplyKeyboardMarkup = ReplyKeyboardMarkup(keyboard=[[button_1, button_2, button_3],
-#                                                                  [button_4, button_5, button_6],
-#                                                                  [button_7, button_8, button_9]],
-#                                                        resize_keyboard=True)
+my_keyboard: ReplyKeyboardMarkup = ReplyKeyboardMarkup(keyboard=[[button_1, button_2, button_3],
+                                                                 [button_4, button_5, button_6],
+                                                                 [button_7, button_8, button_9]],
+                                                       resize_keyboard=True)
+
 
 # Пример 2 - Создаем клавиатуру из 3-х рядов кнопок по 3 кнопки в каждом ряду
 # Создаем объекты кнопок
@@ -68,9 +69,9 @@ dp: Dispatcher = Dispatcher()
 
 # Пример 6 - 350 рядов кнопок по одной кнопке в каждом ряду
 # Заполняем список списками с кнопками
-keyboard: list[list[KeyboardButton]] = [[KeyboardButton(text=f'Кнопка {i}')] for i in range(1, 351)]
+# keyboard: list[list[KeyboardButton]] = [[KeyboardButton(text=f'Кнопка {i}')] for i in range(1, 351)]
 # Создаем объект клавиатуры, добавляя в него список списков с кнопками
-my_keyboard: ReplyKeyboardMarkup = ReplyKeyboardMarkup(keyboard=keyboard, resize_keyboard=True)
+# my_keyboard: ReplyKeyboardMarkup = ReplyKeyboardMarkup(keyboard=keyboard, resize_keyboard=True)
 
 
 # Этот хэндлер будет срабатывать на команду /start
