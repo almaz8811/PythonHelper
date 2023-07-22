e5615d4c92bd7d33df6099151c7bdc66d3efa41a
almaz8811 <almaz8811@yandex.ru>
2023-07-23 00:38:57 +0300

    Разбивка книги на страницы

--- stepik/aiogram/bookbot/services/file_handling.py
+++ stepik/aiogram/bookbot/services/file_handling.py
@@ -32,10 +32,15 @@ def _get_part_text(text: str, start: int, size: int) -> tuple[str, int]:
 
 # Функция, формирующая словарь книги
 def prepare_book(path: str) -> None:
-    pass
+    with open(path, 'r') as file:
+        text = file.read()
+    start, page_number = 0, 1
+    while start < len(text):
+        page_text, page_size = _get_part_text(text, start, PAGE_SIZE)
+        start += page_size
+        book[page_number] = page_text.strip()
+        page_number += 1
 
 
 # Вызов функции prepare_book для подготовки книги из текстового файла
 prepare_book(os.path.join(os.getcwd(), BOOK_PATH))
-
-print(_get_part_text(s, 0, 26))
