ea7a06e88a2105d7ea12386ccb322d6bf60ef041
AlMaz <almaz8811@gmail.com>
2024-05-08 11:00:38 +0300

    Загрузка и скачивание изображений тремя способами

--- telegram/grusha/messages/messages.py
+++ telegram/grusha/messages/messages.py
@@ -1,6 +1,6 @@
 import logging
-from aiogram import Bot, Dispatcher
-from aiogram.types import Message
+from aiogram import Bot, Dispatcher, F
+from aiogram.types import Message, FSInputFile, URLInputFile, BufferedInputFile
 from aiogram.filters.command import Command, CommandObject
 from config_data.config import Config, load_config
 
@@ -50,6 +50,52 @@ async def cmd_custom1(message: Message):
 @dp.message(Command('custom2', prefix='/!'))
 async def cmd_custom2(message: Message):
     await message.answer('И это тоже команда!')
+
+
+# Хэндлер отправляет пользователю в ответ эту же гифку, используя file_id
+@dp.message(F.animation)
+async def echo_gif(message: Message):
+    await message.reply_animation(message.animation.file_id)
+
+
+# Пример отправки изображений тремя способами FSInputFile, BufferedInputFile, URLInputFile
+@dp.message(Command('images'))
+async def upload_photo(message: Message):
+    # Сюда будем помещать file_id отправленных файлов, чтобы потом ими воспользоваться
+    file_ids = []
+
+    # Чтобы продемонстрировать BufferedInputFile, воспользуемся 'классическим' открытием файла
+    # через 'open()'. Но, вообще говоря, этот способ лучше всего подходит для отправки байтов
+    # из оперативной памяти после проведения каких-либо манипуляций, например, редактированием через Pillow
+    with open('buffer_emulation.jpg', 'rb') as image_from_buffer:
+        result = await message.answer_photo(BufferedInputFile(image_from_buffer.read(),
+                                                              filename='image from buffer.jpg'),
+                                            caption='Изображение из буфера')
+        file_ids.append(result.photo[-1].file_id)
+
+    # Отправка файли из файловой системы
+    image_from_pc = FSInputFile('image_from_pc.jpg')
+    result = await message.answer_photo(image_from_pc, caption='Изображение из файла на компьютере')
+    file_ids.append(result.photo[-1].file_id)
+
+    # Отправка файла по ссылке
+    image_from_url = URLInputFile('https://picsum.photos/seed/groosha/400/300')
+    result = await message.answer_photo(image_from_url, caption='Изображение по ссылке')
+    file_ids.append(result.photo[-1].file_id)
+    await message.answer('Отправленные файлы:\n' + '\n'.join(file_ids))
+
+
+# Скачивание файлов
+@dp.message(F.photo)
+async def download_photo(message: Message, bot: Bot):
+    await bot.download(message.photo[-1],
+                       destination=f'/tmp/{message.photo[-1].file_id}.jpg')
+
+
+@dp.message(F.sticker)
+async def download_sticker(message: Message, bot: Bot):
+    await bot.download(message.sticker,
+                       destination=f'/tmp/{message.sticker.file_id}.webp')
 
 
 if __name__ == '__main__':
